7ccbf8fef87cfb2fb9a90da4c5c095706e0894df
Nathan Vitiver <nathan.vitiver@gmail.com>
2019-09-03 13:45:52 -0300

    Formata a lista de produtos para o Excel

---
+++ listaProdutos.py
@@ -0,0 +1,40 @@
+''''
+
+b =('Teste','x','y')
+
+a = []
+
+a.append(b)
+
+print(a)
+
+c = ('z','a','b')
+
+a.append(c)
+
+print(a)
+
+print(a[1][2])
+
+
+'''
+
+from escreveExcel import *
+lista =[]
+i = 0
+j = 0
+#Lista os produtos e guarda
+def listaProdutos(data, nome, municipio, numNF, nat, natureza, fornecedor, cfop, pisconfins, vICMS, zero, vProd):
+    b =(data, nome, municipio, numNF, nat, natureza, fornecedor, cfop, pisconfins, vICMS, zero, zero, vProd, vProd, zero, vProd)
+
+    lista.append(b)
+
+
+def mostraLista(Venc):
+    #print(lista[i][j])
+    x = len(lista)
+    i = 0
+    while i < x:
+        #            id, data,        nome,       municipio,   numNF,       nat,         natureza,    fornecedor,  cfop,        pisconfins,  vICMS,       v1,           v2,           vProd,        vProd2,       v3,           vProd3,       Venc
+        lancaPlanilha(i, lista[i][0], lista[i][1],lista[i][2], lista[i][3], lista[i][4], lista[i][5], lista[i][6], lista[i][7], lista[i][8], lista[i][9], lista[i][10], lista[i][11], lista[i][12], lista[i][13], lista[i][14], lista[i][15], Venc)
+        i+=1
